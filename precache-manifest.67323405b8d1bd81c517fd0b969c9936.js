dcc1d1941002ac13c5588608dd4ebba6708f8102
Puneet <birdipuneet@gmail.com>
2020-05-04 08:29:21 -0400

    Updates

---
+++ precache-manifest.67323405b8d1bd81c517fd0b969c9936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdea80af8ad5cf51aee8aee2ed0a333",
+    "url": "/speedreader/index.html"
+  },
+  {
+    "revision": "a3cd219aa5f6f29eac03",
+    "url": "/speedreader/static/css/main.1b3764d4.chunk.css"
+  },
+  {
+    "revision": "7d05966a184ccad150f9",
+    "url": "/speedreader/static/js/2.ed21c278.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/speedreader/static/js/2.ed21c278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cd219aa5f6f29eac03",
+    "url": "/speedreader/static/js/main.e1b7ca09.chunk.js"
+  },
+  {
+    "revision": "7a324e133ab94d3476f2",
+    "url": "/speedreader/static/js/runtime-main.9278a42d.js"
+  }
+]);
